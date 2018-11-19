015b077cdb0e3071ecfb6aaa5467b8c8388219e1
monnier.charly@orange.fr <32869761+charly33@users.noreply.github.com>
2018-11-19 16:47:51 +0100

    particles

--- script.js
+++ script.js
@@ -1,11 +1,63 @@
-class Person
+/* Set up */
+const $canvas = document.querySelector('.js-canvas')
+const context = $canvas.getContext('2d')
+
+/* Resize */
+const sizes = {width: 800, height: 600}
+
+const resize = () =>
 {
-    constructor()
-    {
-        console.log('new person')
-    }
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    $canvas.width = sizes.width
+    $canvas.height = sizes.height
 }
 
-const personA = new Person()
-const personB = new Person()
-const personC = new Person()
+window.addEventListener('resize', resize)
+resize()
+
+/* Cursor */
+const cursor = {x: 0, y: 0, down: false}
+
+window.addEventListener('mousemove', (_event) =>
+{
+    cursor.x =_event.clientX
+    cursor.y =_event.clientY
+})
+window.addEventListener('mousedown',() =>
+{
+    cursor.down = true
+})
+window.addEventListener('mouseup',() =>
+{
+    cursor.down = false
+})
+/* Particules */
+let particles = []
+/* Animation */
+const loop = ()=>
+{
+    window.requestAnimationFrame(loop)
+    // Particles
+    if(cursor.down)
+    {
+        const particle = new Particle(cursor.x, cursor.y, context, sizes)
+        particles.push(particle)
+    }
+    
+     // Clear
+     context.fillStyle = '#222222'
+     context.fillRect(0, 0, sizes.width, sizes.height)
+    
+     //draw particles
+     for(const _particle of particles)
+     {
+         _particle.draw()
+     }
+     particles = particles.filter((_particle)=>
+    {
+        return !_particle.isOut
+    })
+}
+loop()
